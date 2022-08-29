c2dbf01b952ed77fd3cd64601579d10972a97a18
user <toropov.nikita2004@gmail.com>
2022-08-29 13:00:53 +0200

    first commint

---
+++ index.js
@@ -0,0 +1,28 @@
+let notes = 0;
+function createElements(){
+    if(document.getElementById("textarea").value === "") return;
+    let div = document.createElement("div");
+    let title = document.createElement("h4");
+    let content = document.createElement("p");
+    let button = document.createElement("button");
+    button.innerHTML = "Delete";
+    button.setAttribute("value", ++notes);
+    button.addEventListener("click", () => {
+        document.getElementById(button.value).remove();
+        document.getElementById("textarea").focus();
+    });
+    div.setAttribute("id", notes);
+    let date = new Date();
+    title.innerHTML = date.getDay()+"/" + date.getMonth() + "/" + date.getFullYear() + ", "
+        + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    content.innerHTML = document.getElementById("textarea").value;
+    addElements(div, title, content, button);
+}
+function addElements(div, title, content, button){
+    div.append(title);
+    div.append(button);
+    div.append(content);
+    document.getElementById("notes").appendChild(div);
+    document.getElementById("textarea").value = "";
+    document.getElementById("textarea").focus();
+}
